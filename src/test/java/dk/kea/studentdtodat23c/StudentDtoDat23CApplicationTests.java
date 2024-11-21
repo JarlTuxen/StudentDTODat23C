13fcfa311b64ba2eda9929ece501d6eebe14f3c3
Jarl Tuxen <jart@kea.dk>
2024-11-21 17:53:40 +0100

    StudentRepositoryTest

--- src/test/java/dk/kea/studentdtodat23c/StudentDtoDat23CApplicationTests.java
+++ src/test/java/dk/kea/studentdtodat23c/StudentDtoDat23CApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StudentDtoDat23CApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
---
+++ src/test/java/dk/kea/studentdtodat23c/service/StudentServiceTest.java
@@ -0,0 +1,143 @@
+package dk.kea.studentdtodat23c.service;
+
+import dk.kea.studentdtodat23c.dto.StudentRequestDTO;
+import dk.kea.studentdtodat23c.dto.StudentResponseDTO;
+import dk.kea.studentdtodat23c.model.Student;
+import dk.kea.studentdtodat23c.repository.StudentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doThrow;
+
+@SpringBootTest
+class StudentServiceTest {
+
+    @Mock
+    private StudentRepository mockedStudentRepository;
+
+    //autowired dependencies in studentservice does not initialise when using InjectMocks
+    //@InjectMocks
+    StudentService studentService;
+
+
+    @BeforeEach
+    void init(){
+        Student s1 = new Student();
+        s1.setId(1L);
+        s1.setName("Sigurd");
+        s1.setPassword("Hemmeligt");
+        s1.setBornDate(LocalDate.of(2010,11,12));
+        s1.setBornTime(LocalTime.of(23, 59, 59));
+
+        Student s2 = new Student();
+        s2.setId(2L);
+        s2.setName("Ida");
+        s2.setPassword("Secret");
+        s2.setBornDate(LocalDate.of(2000,1,1));
+        s2.setBornTime(LocalTime.of(0, 0, 1));
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(s1);
+        studentList.add(s2);
+        //findall giver liste af studerende
+        Mockito.when(mockedStudentRepository.findAll()).thenReturn(studentList);
+        //findById giver studerende på id=1 og empty på id=42
+        Mockito.when(mockedStudentRepository.findById(1L)).thenReturn(Optional.of(s1));
+        Mockito.when(mockedStudentRepository.findById(42L)).thenReturn(Optional.empty());
+        //deleteById giver empty hvis id = 42 - doThrow bruges, da deleteById er void
+        //Mockito.when(mockedStudentRepository.deleteById(42)).thenThrow(new StudentNotFoundException("Student not found with id: 42"));
+        doThrow(new RuntimeException("Student not found with id: 42")).when(mockedStudentRepository).deleteById(42L);
+
+        // Define the behavior using thenAnswer
+        // The student passed in save, can be read from arguments in the InvocationOnMock object
+        Mockito.when(mockedStudentRepository.save(ArgumentMatchers.any(Student.class))).thenAnswer(new Answer<Student>() {
+            @Override
+            public Student answer(InvocationOnMock invocation) throws Throwable {
+                // Extract the student object passed as an argument to the save method
+                Object[] arguments = invocation.getArguments();
+                if (arguments.length > 0 && arguments[0] instanceof Student) {
+                    Student studentToSave = (Student) arguments[0];
+                    //er id = 0, så simuler create - er id sat så simuler opdater
+                    if (studentToSave.getId()==null) {
+                        //create - repository skal returnere studentobject med næste ledige id = 3
+                        studentToSave.setId(3L);
+                    }
+                    return studentToSave;
+                } else {
+                    // Handle the case where the argument is not a Student (optional)
+                    throw new IllegalArgumentException("Invalid argument type");
+                }
+            }
+        });
+        //inject mocked repository and other dependencies in studentService
+        studentService = new StudentService(mockedStudentRepository);
+
+    }
+
+    @Test
+    void getAllStudents() {
+        //Act
+        List<StudentResponseDTO> studentDTOList = studentService.getAllStudents();
+        //Assert
+        assertEquals("Sigurd", studentDTOList.get(0).name());
+        assertEquals("Ida", studentDTOList.get(1).name());
+    }
+
+    @Test
+    void getStudentById() {
+        //Act
+        StudentResponseDTO studentDTO = studentService.getStudentById(1L);
+        //Assert
+        assertEquals("Sigurd", studentDTO.name());
+        assertThrows(RuntimeException.class, () -> studentService.getStudentById(42L));
+    }
+
+    @Test
+    void createStudent() {
+        //kald studentService.save og assertEquals på studentDTO - husk init Mockito.when.thenReturn
+        StudentResponseDTO resultStudentDTO = studentService.createStudent(
+                new StudentRequestDTO(
+                        "Hugo",
+                        "Secret",
+                        LocalDate.of(2000,1,1),
+                        LocalTime.of(0, 0, 1)
+                ));
+        assertEquals("Hugo", resultStudentDTO.name());
+    }
+
+    @Test
+    void updateStudent() {
+        //kald studentService.save og assertEquals på studentDTO - husk init Mockito.when.thenReturn
+        //lav test for eksisterende og ikke eksisterende student (assertThrows)
+        StudentRequestDTO studentRequestDTO = new StudentRequestDTO(
+                "Hugo",
+                "Secret",
+                        LocalDate.of(2000,1,1),
+                        LocalTime.of(0, 0, 1));
+        StudentResponseDTO resultStudentDTO = studentService.updateStudent(1L,  studentRequestDTO);
+        assertEquals(1, resultStudentDTO.id());
+        assertEquals("Hugo", resultStudentDTO.name());
+        assertThrows(RuntimeException.class, () -> studentService.updateStudent(42L, studentRequestDTO));
+    }
+
+    @Test
+    void deleteStudentById() {
+        //kald studentService.delete
+        //test at der kommer StudentNotFoundException for ikke-eksisterende student
+        assertThrows(RuntimeException.class, () -> studentService.deleteStudent(42L));
+    }
+}
